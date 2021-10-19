16c668de6f369e806cc309910ee2e752540c58ec
Hugo Devillers <hugo.devillers@gmail.com>
2021-10-19 18:15:09 +0200

    Add a function to load sequences in a hash ref.

--- utils/utils.go
+++ utils/utils.go
@@ -28,3 +28,25 @@ func LoadSeqInArray(i, f string, a *[]seq.Seq) int {
 
 	return nseq
 }
+
+/*
+	Load sequences in a ref hash (indexed by sequence ID)
+*/
+func LoadSeqInMap(i, f string, m *map[string]seq.Seq) int {
+	nseq := 0
+	isgz := false
+	if regexp.MustCompile(`\.gz$`).MatchString(i) {
+		isgz = true
+	}
+	reader := seqio.NewReader(i, f, isgz)
+	reader.CheckPanic()
+	defer reader.Close()
+
+	for reader.Next() {
+		reader.CheckPanic()
+		s := reader.Seq()
+		(*m)[s.Id] = s
+		nseq++
+	}
+	return nseq
+}
